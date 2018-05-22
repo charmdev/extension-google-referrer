fd5e463b7e93411b287d8575408acac5eac16d34
smival <valentin.smirnov@nevosoft.ru>
2018-05-22 19:20:18 +0300

    try to fix build errors

--- dependencies/android/src/org/haxe/extension/GoogleReferrer.java
+++ dependencies/android/src/org/haxe/extension/GoogleReferrer.java
@@ -11,34 +11,12 @@ import android.view.View;
 import android.net.Uri;
 import android.util.Log;
 
+import com.android.installreferrer.api.InstallReferrerStateListener;
+import com.android.installreferrer.api.InstallReferrerClient;
+import com.android.installreferrer.api.ReferrerDetails;
+import com.android.installreferrer.api.InstallReferrerClient.InstallReferrerResponse;
 
-/* 
-	You can use the Android Extension class in order to hook
-	into the Android activity lifecycle. This is not required
-	for standard Java code, this is designed for when you need
-	deeper integration.
-	
-	You can access additional references from the Extension class,
-	depending on your needs:
-	
-	- Extension.assetManager (android.content.res.AssetManager)
-	- Extension.callbackHandler (android.os.Handler)
-	- Extension.mainActivity (android.app.Activity)
-	- Extension.mainContext (android.content.Context)
-	- Extension.mainView (android.view.View)
-	
-	You can also make references to static or instance methods
-	and properties on Java classes. These classes can be included 
-	as single files using <java path="to/File.java" /> within your
-	project, or use the full Android Library Project format (such
-	as this example) in order to include your own AndroidManifest
-	data, additional dependencies, etc.
-	
-	These are also optional, though this example shows a static
-	function for performing a single task, like returning a value
-	back to Haxe from Java.
-*/
-public class Extension_google_referrer extends Extension {
+public class GoogleReferrer extends Extension {
 
 	static final String TAG = "GOOGLE-REF-EXTENSION";
 	static InstallReferrerClient mReferrerClient;
@@ -69,7 +47,7 @@ public class Extension_google_referrer extends Extension {
 	 */
 	public void onCreate (Bundle savedInstanceState) {
 
-		mReferrerClient = InstallReferrerClient.newBuilder(this).build();
+		mReferrerClient = InstallReferrerClient.newBuilder(mainContext).build();
 		mReferrerClient.startConnection(new InstallReferrerStateListener() {
 			@Override
 			public void onInstallReferrerSetupFinished(int responseCode) {
